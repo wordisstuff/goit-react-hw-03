12595f48b8de38db42a3192a1ddf4704b09794fe
wordisstuff <wordisstuff@gmail.com>
2024-03-24 23:18:11 -0500

    3

--- src/components/helpers/validationSchema.js
+++ src/components/helpers/validationSchema.js
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
 userName: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("*"),
-userTel: Yup.string().matches(/^[0-9]{10}$/,'Мобільний номер повинен містити 10 цифр').required('*'),
+userTel: Yup.string().matches(/^[0-9]{10}$/,'Номер має містити 10 цифр').required('*'),
 });
 
-export default validationSchema``
+export default validationSchema;
